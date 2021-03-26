857392a992dd605a9fb51abd876d2a01008036df
Ashvin <ashvinsrvstv@gmail.com>
2021-03-27 00:23:32 +0530

    first commit

--- api/urls.py
+++ api/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
+from django.conf.urls import url,include
 from myapp import views 
+from rest_framework.routers import DefaultRouter
 
+router=DefaultRouter()
+router.register('stu_create',views.StudentModelViewSet,basename='stu_create')
 urlpatterns = [
+    url('',include(router.urls)),
     url('admin/', admin.site.urls),
-    url('^stu_create/$',views.student_api),
+    #url('^stu_create/$',views.student_api),
     # url('^stu_update/$',views.student_update)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,46 +1,11 @@
-from django.shortcuts import render
-# from rest_framework.parsers import JSONParser
+from rest_framework import viewsets
 from myapp.serializers import StudentSerializers
-# from rest_framework.renderers import JSONRenderer
-# from django.http import HttpResponse
-# from django.views.decorators.csrf import csrf_exempt
-# import io
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
 from myapp.models import Student
 
-@api_view(['GET','POST','PUT','DELETE'])
-def student_api(request):
-    if request.method=='GET':
-        clas=request.data.get('clas')
-        if clas is not None:
-            stu=Student.objects.get(clas=clas)
-            serializer=StudentSerializers(stu)
-            return Response(serializer.data)
-        stu=Student.objects.all()
-        serializer=StudentSerializers(stu,many=True)
-        return Response(serializer.data)
-    if request.method=='POST':
-        serializer=StudentSerializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'msg':'Data Created!!'})
-        return Response(serializer.errors)
-    if request.method=='PUT':
-        clas=request.data.get('clas')
-        stu=Student.objects.get(clas=clas)
-        serializer=StudentSerializers(stu,data=request.data,partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'msg':'Data Updated'})
-        return Response(serializers.errors)
-    if request.method=='DELETE':
-        clas=request.data.get('clas')
-        stu=Student.objects.get(clas=clas)
-        stu.delete()
-        return Response({'msg':'Deleted'})
-
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializers
 
 
 
